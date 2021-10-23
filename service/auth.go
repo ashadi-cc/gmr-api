1c22d971cdc8d39c725ad4dc7b12d7e268f7223a
ashadi <ashadi.cc@gmail.com>
2021-10-23 11:27:28 +0700

    mock:update user testing mocks

--- service/auth.go
+++ service/auth.go
@@ -1,3 +1,5 @@
+//go:generate mockery --name=IAuthService
+
 package service
 
 import (
--- store/repository/test/mock_user.go
+++ store/repository/test/mock_user.go
@@ -30,6 +30,13 @@ func NewMockUser(t *testing.T) *User {
 	}
 }
 
+func (user *User) assertionMethodCall(method string, expectedCalls int, arguments ...interface{}) {
+	if expectedCalls > 0 {
+		user.AssertCalled(user.t, method, arguments...)
+	}
+	user.AssertNumberOfCalls(user.t, method, expectedCalls)
+}
+
 //ListenOnFindByUsername listening FindByUsername call and return values from args
 func (user *User) ListenOnFindByUsername(returnUser repository.UserModel, returnErr error) *User {
 	user.On(FindByUsernameMethod, mock.Anything, mock.IsType(string(""))).Return(returnUser, returnErr)
@@ -38,9 +45,30 @@ func (user *User) ListenOnFindByUsername(returnUser repository.UserModel, return
 
 //AssertFindByUsernameCall assertion FindByUsername call
 func (user *User) AssertFindByUsernameCall(expectedCalls int, arguments ...interface{}) *User {
-	if expectedCalls > 0 {
-		user.AssertCalled(user.t, FindByUsernameMethod, arguments...)
-	}
-	user.AssertNumberOfCalls(user.t, FindByUsernameMethod, expectedCalls)
+	user.assertionMethodCall(FindByUsernameMethod, expectedCalls, arguments...)
+	return user
+}
+
+//ListenOnFindByUserID listening FindByUserID call and returns values from args
+func (user *User) ListenOnFindByUserID(returnUser repository.UserModel, returnErr error) *User {
+	user.On(FindByUserIDMethod, mock.Anything, mock.IsType(int(0))).Return(returnUser, returnErr)
+	return user
+}
+
+//AssertFindByUserIDCall assertion FindByUserID call
+func (user *User) AssertFindByUserIDCall(expectedCalls int, arguments ...interface{}) *User {
+	user.assertionMethodCall(FindByUserIDMethod, expectedCalls, arguments...)
+	return user
+}
+
+//ListenOnUpdateEmailandPassword listening on UpdateEmailandPassword call and returns values from args
+func (user *User) ListenOnUpdateEmailandPassword(returnErr error) *User {
+	user.On(UpdateEmailandPasswordMethod, mock.Anything, mock.Anything).Return(returnErr)
+	return user
+}
+
+//AssertUpdateEmailandPasswordCall assertion UpdateEmailandPassword call
+func (user *User) AssertUpdateEmailandPasswordCall(expectedCalls int, arguments ...interface{}) *User {
+	user.assertionMethodCall(UpdateEmailandPasswordMethod, expectedCalls, arguments...)
 	return user
 }
